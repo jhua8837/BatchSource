30e0c7eb7a94865c9126187579789c290038005a
meschac1993 <meschac1993>
2019-07-15 20:51:02 +0000

    added week 1 and ProjectMeschac

---
+++ java/ProjectMeschac/src/com/revature/driver/Driver.java
@@ -0,0 +1,34 @@
+package com.revature.driver;
+// package -namespace to organize related class/Interfaces,ect
+
+public class Driver {
+// single line comment
+	/*
+	 * 
+	 * This
+	 * is
+	 * a 
+	 * really
+	 * long
+	 * multi
+	 * line
+	 * comment
+	 */
+	/*
+	 * Naming Conventions 
+	 * Classes and projects-pascal- Capitalize each word 
+	 * methods and varaiables -camelCase ex firstSecondThird
+	 * Package names:all lowercase ,separated by periods 
+	 * constants :ALL_CAPS_LOLZ
+	 * 
+	 */
+	// Main Method is the entry point 
+	public static void main(String[]args  ) {
+		/*
+		 * public- access modifiers, anything can see it 
+		 * static- belongs to class , does not need to be instantiated 
+		 * String []args-parameter utilized in this method 
+		 */
+		 System.out.println("Goodbye ,earth");
+	}
+}
